e20e38c02d5160eaa7633ddcf2e3a806e9b03c7b
Akihiro Saiki <misly.lx00@gmail.com>
2022-07-28 01:57:31 +0900

    impl HOG calculation (partial)

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,15 @@ use std::fs::{self, DirEntry};
 use std::path::Path;
 use std::{error, fmt};
 use std::result;
-use opencv::prelude::*;
-use opencv::ml::prelude::*;
-use opencv::ml::SVM;
+use opencv::{
+    core::*,
+    Result,
+    prelude::*,
+    imgcodecs,
+    imgproc,
+    ml::prelude::*,
+    ml::SVM,
+};
 
 
 #[derive(Debug)]
@@ -52,12 +58,80 @@ fn get_files(path_str: &str) -> result::Result<Vec<String>, ModelCreationError> 
     Ok(files)
 }
 
-fn create_svm_model(path_str: &str) {
+fn create_svm_model(path_str: &str) -> Result<()> {
+    /* Get images' path from directory */
     let train_filenames = match get_files(path_str) {
         Ok(vec) => vec,
-        Err(e) => panic!("Error: {}", e) 
+        Err(e) => panic!("Error: {}", e), 
     };
-    
+
+    /* Processing images */
+    for imgfile in train_filenames {
+        /* Load images */
+        let img = imgcodecs::imread(&imgfile, imgcodecs::IMREAD_COLOR).unwrap();
+
+        /* Grayscale */
+        let mut gray_scale = Mat::default();
+        imgproc::cvt_color(
+            &img,
+            &mut gray_scale,
+            imgproc::COLOR_BGR2GRAY, 
+            0
+        )?;
+
+        /* Downscale to 64x64 */
+        let mut downscale = Mat::default();
+        imgproc::resize(&gray_scale, 
+            &mut downscale, 
+            Size::new(64, 64), 
+            0.0, 
+            0.0, 
+            imgproc::INTER_LINEAR
+        )?;
+
+        /* Apply sobel filter 
+         * to get edge image
+         */
+        let mut edge_x = Mat::default();
+        let mut edge_y = Mat::default();
+        imgproc::sobel(
+            &downscale,
+            &mut edge_x,
+            CV_32F,
+            1,
+            0,
+            3,
+            1.0,
+            0.0,
+            BORDER_DEFAULT
+        )?;
+        imgproc::sobel(
+            &downscale,
+            &mut edge_y,
+            CV_32F,
+            0,
+            1,
+            3,
+            1.0,
+            0.0,
+            BORDER_DEFAULT
+        )?;
+
+        /* Calc magnitude and angle of edge gradient */
+        let mut magnitude = Mat::default();
+        let mut angle = Mat::default();
+        cart_to_polar(
+            &edge_x,
+            &edge_y,
+            &mut magnitude,
+            &mut angle,
+            false
+        )?;
+
+        /* Quantization */
+
+    } 
+    Ok(())
 }
 
 fn main() {
